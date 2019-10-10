86e8a093b9a22d23ba43037db9a52a9e79142463
Ravid Hausmann <32935577+RavidHausmann@users.noreply.github.com>
2019-10-10 13:26:15 +0200

    Add Trend functionality

--- 1509 Stock Prediction.py
+++ 1509 Stock Prediction.py
@@ -7,15 +7,35 @@ Created on Sun Sep 15 11:57:48 2019
 import requests
 import json
 import matplotlib.pyplot as plt
-import math
 import numpy
+"""this iteration looks like its performing fucking insanely good, but its not refactored yet, so pretty unreadable, and its also having some display errros (multiple sells after another)"""
+
+def Trend(List):
+    return List[-1]-List[0]
+
+def Var(List):
+    avg=sum(List)/len(List)
+    return sum([numpy.square(x-avg) for x in List])/len(List)
+
+
+def NEGmoddedOST(List):
+    stop=int(len(List)/numpy.e)
+    Estimate=min(List[:stop])
+    avg=sum(List[:stop])/stop
+    for i in range(stop,len(List)):
+        Entry=List[i]
+        Needed=avg+(Estimate-avg)*(1-(i-stop)/(len(List)-stop))
+        if Entry<Needed:
+            return Entry,i
+    return Entry,i
 
 def moddedOST(List):
     stop=int(len(List)/numpy.e)
     Estimate=max(List[:stop])
+    avg=sum(List[:stop])/stop
     for i in range(stop,len(List)):
         Entry=List[i]
-        Needed=Estimate*(1-(i-stop)/(len(List)-stop))
+        Needed=avg+(Estimate-avg)*(1-(i-stop)/(len(List)-stop))
         if Entry>Needed:
             return Entry,i
     return Entry,i
@@ -56,20 +76,36 @@ i=0
 Periode=200
 Graph=[]
 while i<(len(Werte)-Periode):
-    BuyPreis,BuyPunkt=moddedOST(numpy.multiply(Werte[i:i+Periode],-1))
-    BuyPreis*=-1
-    i+=BuyPunkt
-    Buy(BuyPreis,TradePrice)
-    
-    XB.append(i)
-    YB.append(BuyPreis)
+    if Trend(Werte[i:i+Periode])>2*Var(Werte[i:i+Periode]):
+        Buy(Werte[i],TradePrice)
+        XB.append(i)
+        YB.append(Werte[i])
+        print('Waiting to sell in Period:',i)
+        while Trend(Werte[i:i+Periode])>2*numpy.sqrt(Var(Werte[i:i+Periode])):
+            i+=1
+        print(i)
+    else:
+        BuyPreis,BuyPunkt=NEGmoddedOST(Werte[i:i+Periode])
+        i+=BuyPunkt
+        Buy(BuyPreis,TradePrice)
+        XB.append(i)
+        YB.append(BuyPreis)
     if i>(len(Werte)-Periode):
         continue
-    SellPreis,SellPunkt=moddedOST(Werte[i:i+Periode])
-    i+=SellPunkt
-    Sell(SellPreis,TradePrice)
-    XS.append(i)
-    YS.append(SellPreis)
+    if Trend(Werte[i:i+Periode])<-2*numpy.sqrt(Var(Werte[i:i+Periode])):
+        Sell(Werte[i],TradePrice)
+        XS.append(i)
+        YS.append(Werte[i])
+        print('Waiting to Buy in Period',i)
+        while Trend(Werte[i:i+Periode])<-2*numpy.sqrt(Var(Werte[i:i+Periode])):
+            i+=1
+        print(i)
+    else:
+        SellPreis,SellPunkt=moddedOST(Werte[i:i+Periode])
+        i+=SellPunkt
+        Sell(SellPreis,TradePrice)
+        XS.append(i)
+        YS.append(SellPreis)
     Graph.append(Guthaben)
     
 plt.subplot(2,2,1)
@@ -88,8 +124,8 @@ plt.plot([8000 for i in Graph])
 
 plt.subplot(2,2,3)
 plt.title("Fourier Noise Reduction")
-plt.plot(numpy.fft.ifft([0 if abs(x)<10000 else x for x in numpy.fft.rfft(Werte)]))
+plt.plot(numpy.fft.ifft([0 if abs(x)<1000 else x for x in numpy.fft.rfft(Werte)]))
 
 plt.subplot(2,2,4)
-plt.plot([B-S for B,S in zip(YB,YS)])
+plt.hist([S-B for B,S in zip(YB,YS)])
 plt.title("Decission Effectiveness")
